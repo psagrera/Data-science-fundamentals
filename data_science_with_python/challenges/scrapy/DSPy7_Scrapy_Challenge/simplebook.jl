c99e470689b3cd8c3b2e2c85f22e69a7594ce091
psagrera <psagrerag@gmail.com>
2020-05-21 16:53:50 +0200

    added new files and new folders

---
+++ data_science_with_python/challenges/scrapy/DSPy7_Scrapy_Challenge/simplebook.jl
@@ -0,0 +1,60 @@
+{"title": "A Light in the Attic", "price": "\u00a351.77", "rating": "Three", "page": 1}
+{"title": "Tipping the Velvet", "price": "\u00a353.74", "rating": "One", "page": 1}
+{"title": "Soumission", "price": "\u00a350.10", "rating": "One", "page": 1}
+{"title": "Sharp Objects", "price": "\u00a347.82", "rating": "Four", "page": 1}
+{"title": "Sapiens: A Brief History of Humankind", "price": "\u00a354.23", "rating": "Five", "page": 1}
+{"title": "The Requiem Red", "price": "\u00a322.65", "rating": "One", "page": 1}
+{"title": "The Dirty Little Secrets of Getting Your Dream Job", "price": "\u00a333.34", "rating": "Four", "page": 1}
+{"title": "The Coming Woman: A Novel Based on the Life of the Infamous Feminist, Victoria Woodhull", "price": "\u00a317.93", "rating": "Three", "page": 1}
+{"title": "The Boys in the Boat: Nine Americans and Their Epic Quest for Gold at the 1936 Berlin Olympics", "price": "\u00a322.60", "rating": "Four", "page": 1}
+{"title": "The Black Maria", "price": "\u00a352.15", "rating": "One", "page": 1}
+{"title": "Starving Hearts (Triangular Trade Trilogy, #1)", "price": "\u00a313.99", "rating": "Two", "page": 1}
+{"title": "Shakespeare's Sonnets", "price": "\u00a320.66", "rating": "Four", "page": 1}
+{"title": "Set Me Free", "price": "\u00a317.46", "rating": "Five", "page": 1}
+{"title": "Scott Pilgrim's Precious Little Life (Scott Pilgrim #1)", "price": "\u00a352.29", "rating": "Five", "page": 1}
+{"title": "Rip it Up and Start Again", "price": "\u00a335.02", "rating": "Five", "page": 1}
+{"title": "Our Band Could Be Your Life: Scenes from the American Indie Underground, 1981-1991", "price": "\u00a357.25", "rating": "Three", "page": 1}
+{"title": "Olio", "price": "\u00a323.88", "rating": "One", "page": 1}
+{"title": "Mesaerion: The Best Science Fiction Stories 1800-1849", "price": "\u00a337.59", "rating": "One", "page": 1}
+{"title": "Libertarianism for Beginners", "price": "\u00a351.33", "rating": "Two", "page": 1}
+{"title": "It's Only the Himalayas", "price": "\u00a345.17", "rating": "Two", "page": 1}
+{"title": "In Her Wake", "price": "\u00a312.84", "rating": "One", "page": 2}
+{"title": "How Music Works", "price": "\u00a337.32", "rating": "Two", "page": 2}
+{"title": "Foolproof Preserving: A Guide to Small Batch Jams, Jellies, Pickles, Condiments, and More: A Foolproof Guide to Making Small Batch Jams, Jellies, Pickles, Condiments, and More", "price": "\u00a330.52", "rating": "Three", "page": 2}
+{"title": "Chase Me (Paris Nights #2)", "price": "\u00a325.27", "rating": "Five", "page": 2}
+{"title": "Black Dust", "price": "\u00a334.53", "rating": "Five", "page": 2}
+{"title": "Birdsong: A Story in Pictures", "price": "\u00a354.64", "rating": "Three", "page": 2}
+{"title": "America's Cradle of Quarterbacks: Western Pennsylvania's Football Factory from Johnny Unitas to Joe Montana", "price": "\u00a322.50", "rating": "Three", "page": 2}
+{"title": "Aladdin and His Wonderful Lamp", "price": "\u00a353.13", "rating": "Three", "page": 2}
+{"title": "Worlds Elsewhere: Journeys Around Shakespeare\u2019s Globe", "price": "\u00a340.30", "rating": "Five", "page": 2}
+{"title": "Wall and Piece", "price": "\u00a344.18", "rating": "Four", "page": 2}
+{"title": "The Four Agreements: A Practical Guide to Personal Freedom", "price": "\u00a317.66", "rating": "Five", "page": 2}
+{"title": "The Five Love Languages: How to Express Heartfelt Commitment to Your Mate", "price": "\u00a331.05", "rating": "Three", "page": 2}
+{"title": "The Elephant Tree", "price": "\u00a323.82", "rating": "Five", "page": 2}
+{"title": "The Bear and the Piano", "price": "\u00a336.89", "rating": "One", "page": 2}
+{"title": "Sophie's World", "price": "\u00a315.94", "rating": "Five", "page": 2}
+{"title": "Penny Maybe", "price": "\u00a333.29", "rating": "Three", "page": 2}
+{"title": "Maude (1883-1993):She Grew Up with the country", "price": "\u00a318.02", "rating": "Two", "page": 2}
+{"title": "In a Dark, Dark Wood", "price": "\u00a319.63", "rating": "One", "page": 2}
+{"title": "Behind Closed Doors", "price": "\u00a352.22", "rating": "Four", "page": 2}
+{"title": "You can't bury them all: Poems", "price": "\u00a333.63", "rating": "Two", "page": 2}
+{"title": "Slow States of Collapse: Poems", "price": "\u00a357.31", "rating": "Three", "page": 3}
+{"title": "Reasons to Stay Alive", "price": "\u00a326.41", "rating": "Two", "page": 3}
+{"title": "Private Paris (Private #10)", "price": "\u00a347.61", "rating": "Five", "page": 3}
+{"title": "#HigherSelfie: Wake Up Your Life. Free Your Soul. Find Your Tribe.", "price": "\u00a323.11", "rating": "Five", "page": 3}
+{"title": "Without Borders (Wanderlove #1)", "price": "\u00a345.07", "rating": "Two", "page": 3}
+{"title": "When We Collided", "price": "\u00a331.77", "rating": "One", "page": 3}
+{"title": "We Love You, Charlie Freeman", "price": "\u00a350.27", "rating": "Five", "page": 3}
+{"title": "Untitled Collection: Sabbath Poems 2014", "price": "\u00a314.27", "rating": "Four", "page": 3}
+{"title": "Unseen City: The Majesty of Pigeons, the Discreet Charm of Snails & Other Wonders of the Urban Wilderness", "price": "\u00a344.18", "rating": "Four", "page": 3}
+{"title": "Unicorn Tracks", "price": "\u00a318.78", "rating": "Three", "page": 3}
+{"title": "Unbound: How Eight Technologies Made Us Human, Transformed Society, and Brought Our World to the Brink", "price": "\u00a325.52", "rating": "One", "page": 3}
+{"title": "Tsubasa: WoRLD CHRoNiCLE 2 (Tsubasa WoRLD CHRoNiCLE #2)", "price": "\u00a316.28", "rating": "One", "page": 3}
+{"title": "Throwing Rocks at the Google Bus: How Growth Became the Enemy of Prosperity", "price": "\u00a331.12", "rating": "Three", "page": 3}
+{"title": "This One Summer", "price": "\u00a319.49", "rating": "Four", "page": 3}
+{"title": "Thirst", "price": "\u00a317.27", "rating": "Five", "page": 3}
+{"title": "The Torch Is Passed: A Harding Family Story", "price": "\u00a319.09", "rating": "One", "page": 3}
+{"title": "The Secret of Dreadwillow Carse", "price": "\u00a356.13", "rating": "One", "page": 3}
+{"title": "The Pioneer Woman Cooks: Dinnertime: Comfort Classics, Freezer Food, 16-Minute Meals, and Other Delicious Ways to Solve Supper!", "price": "\u00a356.41", "rating": "One", "page": 3}
+{"title": "The Past Never Ends", "price": "\u00a356.50", "rating": "Four", "page": 3}
+{"title": "The Natural History of Us (The Fine Art of Pretending #2)", "price": "\u00a345.22", "rating": "Three", "page": 3}
